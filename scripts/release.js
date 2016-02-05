ca319dbf3ddca7999b72644b6abe642ec8e5075c
Krasimir Tsonev <krasimir@outset.ws>
2016-02-05 07:55:11 +0200

    Releasing a new version

--- scripts/release.js
+++ scripts/release.js
@@ -1,8 +1,11 @@
 var readline = require('readline');
 var fs = require('fs');
 
-var client = require('../packages/client/package.json');
-var transpiler = require('../packages/transpiler/package.json');
+var clientFile = __dirname + '/../packages/client/package.json';
+var transpilerFile = __dirname + '/../packages/transpiler/package.json';
+
+var client = require(clientFile);
+var transpiler = require(transpilerFile);
 
 if (client.version !== transpiler.version) {
   throw new Error('Client and Transpiler have different versions: ' + client.version + ', ' + transpiler.version);
@@ -18,8 +21,8 @@ reader.question('Type a new version. The current one is ' + client.version + '.\
   client.version = answer;
   transpiler.version = answer;
 
-  fs.writeFileSync('../packages/client/package.json', JSON.stringify(client, null, 2));
-  fs.writeFileSync('../packages/transpiler/package.json', JSON.stringify(transpiler, null, 2));
+  fs.writeFileSync(clientFile, JSON.stringify(client, null, 2));
+  fs.writeFileSync(transpilerFile, JSON.stringify(transpiler, null, 2));
 
   console.log('The version changed to ' + answer);
   reader.close();
