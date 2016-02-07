5b8bbb659bf8d73eb8bc2d599cc9db0bafe1b087
Krasimir Tsonev <krasimir@outset.ws>
2016-02-07 19:35:05 +0200

    Updating webpack example

--- playground/transpiler-webpack/src/ballAnimation.js
+++ playground/transpiler-webpack/src/ballAnimation.js
@@ -1,20 +1,20 @@
 var animationApply = 'ball-animation 1s ease infinite alternate';
 var animation = cssx(
-  @-webkit-keyframes ball-animation {
+  @keyframes ball-animation {
     0%   { 
-      transform: translateX(0);
-      -webkit-transform: translateX(0);
+      (w)transform: translateX(0);
     }
     100% {
-      transform: translateX(200px);
-      -webkit-transform: translateX(200px);
+      (w)transform: translateX(200px);
     }
   }
   .ball {
-    (wmo)animation: `animationApply`;
+    (w)animation: `animationApply`;
   }
 );
 
 module.exports = {
-
+  update: function (endPoint) {
+    
+  }
 };
