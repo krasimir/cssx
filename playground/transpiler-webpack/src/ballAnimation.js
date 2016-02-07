5405779b64fbafd2a0676a591408aabad986ce16
Krasimir Tsonev <krasimir@outset.ws>
2016-02-07 19:07:12 +0200

    Updating playground

--- playground/transpiler-webpack/src/ballAnimation.js
+++ playground/transpiler-webpack/src/ballAnimation.js
@@ -10,41 +10,8 @@ var animation = cssx(
       -webkit-transform: translateX(200px);
     }
   }
-  @-moz-keyframes ball-animation {
-    0%   { 
-      transform: translateX(0);
-      -webkit-transform: translateX(0);
-    }
-    100% {
-      transform: translateX(200px);
-      -webkit-transform: translateX(200px);
-    }
-  }
-  @-o-keyframes ball-animation {
-    0%   { 
-      transform: translateX(0);
-      -webkit-transform: translateX(0);
-    }
-    100% {
-      transform: translateX(200px);
-      -webkit-transform: translateX(200px);
-    }
-  }
-  @keyframes ball-animation {
-    0%   { 
-      transform: translateX(0);
-      -webkit-transform: translateX(0);
-    }
-    100% {
-      transform: translateX(200px);
-      -webkit-transform: translateX(200px);
-    }
-  }
   .ball {
-    -webkit-animation: `animationApply`;
-    -moz-animation: `animationApply`;
-    -o-animation: `animationApply`;
-    animation: `animationApply`;
+    (wmo)animation: `animationApply`;
   }
 );
 
