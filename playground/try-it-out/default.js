e838cad9e5a75f5004387f8cf5daae78024bac1b
Krasimir Tsonev <krasimir@outset.ws>
2016-02-10 07:37:44 +0200

    Updating the playground

--- playground/try-it-out/default.js
+++ playground/try-it-out/default.js
@@ -1,1 +1,15 @@
-var DefaultStyles = 'var updateStyles = function(size) {\n  cssx(\n    body {\n      font-size: `size`px;\n    }\n    body h1 {\n      font-size: 2em;\n    }\n    body small {\n      font-size: 0.8em;\n    }\n  );\n}\nupdateStyles(18);';
+var DefaultStyles = "var sheet = cssx(\n\
+  body {\n\
+    margin: 0;\n\
+    padding: 0;\n\
+  }\n\
+);\n\
+\n\
+var size = 10;\n\
+var ratio = 1.5;\n\
+\n\
+sheet.add('p', cssx({\n\
+  font-size: `size`px;\n\
+  line-height: `size*ratio`px;\n\
+  (wmo)transform: transitionX(10px);\n\
+}));";
