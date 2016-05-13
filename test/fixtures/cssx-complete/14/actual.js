fc78039bc299ffb1484e9fcf04875ea10e542fcf
Krasimir Tsonev <krasimir@outset.ws>
2016-05-13 06:11:54 +0300

    Adding a test covering multiple @font-face declarations #9

---
+++ test/fixtures/cssx-complete/14/actual.js
@@ -0,0 +1,15 @@
+var s = <style>
+@font-face  {
+    font-family: Dosis;
+    font-weight: 400;
+    src: './fonts/Dosis-ExtraLight.ttf';
+  }
+@font-face {
+    font-family: Lato;
+    font-weight: 300;
+    src: './fonts/Lato-Hairline.ttf';
+  }
+</style>;
+
+var sheet = cssx();
+sheet.add(s);
