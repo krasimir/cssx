dae716e68569655564398b293fbdf4fd4cd5e447
Krasimir Tsonev <krasimir@outset.ws>
2016-11-23 07:12:36 +0200

    Adding rollup-plugin-cssx to the release script #24

--- scripts/release.js
+++ scripts/release.js
@@ -2,7 +2,15 @@ var readline = require('readline');
 var fs = require('fs');
 
 var currentVersion;
-var packages = ['cssx', 'cssx-transpiler', 'gulp-cssx', 'cssx-loader', 'cssx-cli', 'browserify-cssx'].map(function (p) {
+var packages = [
+  'cssx',
+  'cssx-transpiler',
+  'gulp-cssx',
+  'cssx-loader',
+  'cssx-cli',
+  'browserify-cssx',
+  'rollup-plugin-cssx'
+].map(function (p) {
   var pJSONPath = __dirname + '/../packages/' + p + '/package.json';
 
   return {
